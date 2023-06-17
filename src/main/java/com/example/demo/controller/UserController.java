c4710d2b7c8afa851257c018e7776199b6d6f043
yasidew <it21440922@my.sliit.lk>
2023-06-17 12:01:26 +0530

    commited changes

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/saveUser")
+    public UserDTO saveuser(@RequestBody UserDTO userDTO){
+       return userService.saveUser(userDTO);
+    }
+    @PutMapping("/updateUser")
+    public UserDTO updateuser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+    @DeleteMapping("/deleteUser")
+    public boolean deleteuser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
+    }
+
+
+    @GetMapping("/getUserByUserId/{userID}")
+    public UserDTO getUserByUserID(@PathVariable String userID){
+        return userService.getUserByUserID(userID);
+    }
+
+    @GetMapping("/getUserByUserIDAndAddress/{userID}/{address}")
+    public UserDTO getUserByUserIDAndAddress(@PathVariable String userID, @PathVariable String address){
+        System.out.println("user ID :"+ userID + "user address :"+ address);
+        return userService.getUserByUserIDAndAddress(userID, address);
+
+        // defines a GET request handler that takes a user ID and address as path variables,
+        // prints them to the console, and then delegates to the userService to retrieve a UserDTO object based on the provided ID and address.
+        // The retrieved UserDTO object is then returned as the response from the controller method.
+    }
+}
